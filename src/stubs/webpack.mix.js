984eeeee18187a74d9b2d5e233efa912cfe5ef3d
Unknown <Reece.Farmer@hgacreative.com>
2019-08-19 12:07:31 +0100

    IE11 backwards compatibility to laravel mix

--- src/stubs/webpack.mix.js
+++ src/stubs/webpack.mix.js
@@ -1,15 +1,36 @@
 const mix = require('laravel-mix');
 
+// https://laravel-mix.com/extensions/polyfill
+require('laravel-mix-polyfill');
+
+// https://github.com/scottcharlesworth/laravel-mix-polyfill/issues/15
+// https://warlord0blog.wordpress.com/2018/08/01/i-hate-internet-explorer/
+const TargetsPlugin = require('targets-webpack-plugin');
+
 mix.webpackConfig({
-     resolve: {
-         alias: {
-             RootJsDir: path.resolve(__dirname, 'resources', 'js'),
-             ClassesDir: path.resolve(__dirname, 'resources', 'js', 'classes'),
-             ComponentsDir: path.resolve(__dirname, 'resources', 'js', 'components'),
-         }
-     }
+        plugins: [
+            new TargetsPlugin({
+                browsers: ['last 2 versions', 'chrome >= 41', 'IE 11'],
+            }),
+        ],
+        resolve: {
+            alias: {
+                RootJsDir: path.resolve(__dirname, 'resources', 'js'),
+                ClassesDir: path.resolve(__dirname, 'resources', 'js', 'classes'),
+                ComponentsDir: path.resolve(__dirname, 'resources', 'js', 'components')
+            }
+        }
     })
     .js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   .version()
-   .extract(['vue', 'axios']);
+    .sass('resources/sass/app.scss', 'public/css')
+    .polyfill({
+        enabled: true,
+        useBuiltIns: "usage",
+        targets: {
+            "firefox": "50",
+            "ie": 11
+        },
+        corejs: 3
+    })
+    .version()
+    .extract(['vue', 'axios', 'laravel-mix-polyfill', 'targets-webpack-plugin']);
